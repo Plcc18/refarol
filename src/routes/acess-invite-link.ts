2d38cfe223a8bc18abb913b471b5caa99001a82c
Plcc18 <pedrolucascabral4@gmail.com>
2025-03-15 20:17:15 -0300

    acessinvitelink route created

---
+++ src/routes/acess-invite-link.ts
@@ -0,0 +1,35 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { env } from '../env'
+import { subscribeToEvent } from '../functions/subscribe-to-event'
+
+export const acessInviteLinkRoute: FastifyPluginAsyncZod = async app => {
+  app.get(
+    '/invite/subscriberID',
+    {
+      schema: {
+        summary: 'Acess invite links and redirects user',
+        tags: ['referral'],
+        params: z.object({
+          subscriberID: z.string(),
+        }),
+        response: {
+          201: z.object({
+            subscriberID: z.string(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { subscriberID } = request.params
+
+      console.log(subscriberID)
+
+      const redirectUrl = new URL(env.WEB_URL)
+
+      redirectUrl.searchParams.set('referrer', subscriberID)
+
+      return reply.redirect(redirectUrl.toString(), 302)
+    }
+  )
+}
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,16 @@
-import { fastify } from 'fastify'
 import { fastifyCors } from '@fastify/cors'
-import { 
-  validatorCompiler, 
-  serializerCompiler, 
- type ZodTypeProvider,
-  jsonSchemaTransform
-} from 'fastify-type-provider-zod'
 import { fastifySwagger } from '@fastify/swagger'
 import { fastifySwaggerUi } from '@fastify/swagger-ui'
-import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
+import { fastify } from 'fastify'
+import {
+  type ZodTypeProvider,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
 import { env } from './env'
+import { acessInviteLinkRoute } from './routes/acess-invite-link'
+import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -22,7 +23,7 @@ app.register(fastifySwagger, {
   openapi: {
     info: {
       title: 'Refarol',
-      version: '0.0.1'
+      version: '0.0.1',
     },
   },
   transform: jsonSchemaTransform,
@@ -33,6 +34,7 @@ app.register(fastifySwaggerUi, {
 })
 
 app.register(subscribeToEventRoute)
+app.register(acessInviteLinkRoute)
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP server running')
